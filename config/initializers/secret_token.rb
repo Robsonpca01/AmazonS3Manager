bcf5e5cd748ad874b176739d818112447b51b5d6
Robson Carvalho <robsonpca@gmail.com>
2013-07-18 16:21:41 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AmazonS3Manager::Application.config.secret_token = '812fcdc5fbaa217e205ead929ab04b17e2edf5995265f933ef7f845628fe8b9853963f91b1c1a86f423fca0b5b0d72c435ba544bebe9deb66d8c5e14095e0db7'
